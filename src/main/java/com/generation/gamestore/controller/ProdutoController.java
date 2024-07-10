7b98886aa57c33fcc87bb0f7f732fcf5ab14f4d7
eduardowada <156962925+eduardowada@users.noreply.github.com>
2024-07-10 19:48:13 -0300

    Projeto finalizado

--- src/main/java/com/generation/gamestore/controller/ProdutoController.java
+++ src/main/java/com/generation/gamestore/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.generation.gamestore.controller;
 
 import com.generation.gamestore.model.Produto;
+import com.generation.gamestore.repository.CategoriaRepository;
 import com.generation.gamestore.repository.ProdutoRepository;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
@@ -21,6 +22,9 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     @GetMapping
     public ResponseEntity<List<Produto>> getAll() {
         return ResponseEntity.ok(produtoRepository.findAll());
@@ -40,16 +44,26 @@ public class ProdutoController {
 
     @PostMapping
     public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
-        return ResponseEntity.status(HttpStatus.CREATED)
+        if (categoriaRepository.existsById(produto.getCategoria().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
                 .body(produtoRepository.save(produto));
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
     }
 
     @PutMapping
     public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
-        return produtoRepository.findById(produto.getId())
-                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
-                        .body(produtoRepository.save(produto)))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        if (produtoRepository.existsById(produto.getId())) {
+
+            if (categoriaRepository.existsById(produto.getCategoria().getId()))
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(produtoRepository.save(produto));
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
